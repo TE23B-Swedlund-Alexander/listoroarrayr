f6c40aef42c560eba6f0c2a70e7809f325028039
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2024-11-13 14:21:54 +0100

    done

--- listoroarrayr/Program.cs
+++ listoroarrayr/Program.cs
@@ -1,1 +1,24 @@
-﻿string[] leksaker = ["nalle","docka","träsvärd","boll","bil"];
+﻿using System.Reflection.Metadata;
+Random  rnd = new Random ();
+
+string[] leksaker = ["nalle", "docka", "träsvärd", "boll", "bil"];
+string[] personer = ["alexander", "nilo","dante","jack","lukas"];
+int[] nummer = [0,1,2,3,4,5,6,7,8,9,10];
+List<string> cities = [];
+for (int t = 0; t < 5; t++)
+{
+    int betyg = rnd.Next(0,10);
+Console.WriteLine($"{personer[t]} ger {leksaker[t]} {nummer[betyg]} i betyg");
+}
+string exit="";
+Console.WriteLine("säg en stad eller något liknande skriv exit för att stänga ner");
+while ( exit != "exit")
+{  
+    exit = Console.ReadLine();
+    if (exit != "exit") cities.Add(exit);   
+}
+for(int repeat = 0; repeat<cities.Count; repeat++)
+{
+Console.WriteLine(cities[repeat]);
+}
+Console.ReadLine();
--- listoroarrayr/obj/Debug/net8.0/listoroarrayr.AssemblyInfo.cs
+++ listoroarrayr/obj/Debug/net8.0/listoroarrayr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("listoroarrayr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d012634ec3ddd989615115c126c3321db3c7be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("listoroarrayr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("listoroarrayr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
